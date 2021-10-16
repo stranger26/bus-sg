4f32d4660109ee461e9091f4758ae120fca70c22
Ho Chin Chee <alexho97@hotmail.com>
2021-10-16 21:24:52 +0800

    [bug] vercel adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 /** @type {import('@sveltejs/kit').Config} */
+import vercel from '@sveltejs/adapter-vercel';
+
 const config = {
 	extensions: ['.svelte', '.svlt'],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: vercel()
 	}
 };
 
