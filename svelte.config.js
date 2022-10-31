1c10e1ee705216e11c2039c45fd89a9354c40710
Ho Chin Chee <alexho97@hotmail.com>
2022-10-31 23:38:37 +0800

    [chore] switch to adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@sveltejs/kit').Config} */
 import importAssets from 'svelte-preprocess-import-assets'
-import vercel from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-auto';
 import path from 'path'
 
 const config = {
@@ -9,7 +9,6 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel(),
 		files: {
 			serviceWorker: 'src/service-worker'
 		},
